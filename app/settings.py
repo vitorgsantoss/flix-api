9ff52eea4bbc9b70609eb4ef2fad859eda50b0d4
Vitor Santos <vg452004@gmail.com>
2025-11-01 16:49:24 -0300

    Aumentando lifetime dos tokens de acesso

--- app/settings.py
+++ app/settings.py
@@ -115,6 +115,6 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
